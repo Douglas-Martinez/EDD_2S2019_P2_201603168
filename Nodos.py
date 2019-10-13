315fea2a3e2634f69a0ef78ef41f744dabbab506
Douglas-Martinez <53403994+Douglas-Martinez@users.noreply.github.com>
2019-10-13 15:18:10 -0600

    Nodos
    Nodos de las Estrucutras

---
+++ Nodos.py
@@ -0,0 +1,25 @@
+import hashlib
+
+class Bloque(object):
+    def __init__(self, i=None, t=None, c=None, d=None, p=None):
+        self.INDEX = i
+        self.TIMESTAMP = t
+        self.CLASS = c
+        self.DATA = d
+        self.PREVIOUSHASH = p
+        self.HASH = self.func_Hash()
+        self.anterior = None
+        self.siguiente = None
+
+    def func_Hash(self):
+        return hashlib.sha256(str(self.INDEX).encode() + str(self.TIMESTAMP).encode() + str(self.CLASS).encode() + str(self.DATA).encode() + str(self.PREVIOUSHASH).encode()).hexdigest()
+
+class NodoAVL(object):
+    def __init__(self, c=0, n=''):
+        self.carne = c
+        self.name = n
+        self.height = 0
+        self.fe = 0
+        
+        self.right = None
+        self.left = None
